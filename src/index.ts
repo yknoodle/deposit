cd463a786ae2015be934dcd95c49b8866646effb
damien <damien0325@gmail.com>
2020-12-21 01:56:46 +0800

    integrate swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import {depositRouter} from "./controller/deposit"
-
+import swagger from 'swagger-ui-express'
+import swaggerDocument from './openapi.json'
 let app = express();
 const port = 3000;
 app.use(
     express.json())
 app.use('/deposit', depositRouter)
+app.use('/', swagger.serve, swagger.setup(swaggerDocument))
 app.listen(port, err => {
     if (err) {
         return console.error(err);
